32f8e20a63c738ef7350b1174b319da77f7ac132
ayoung001 <asy001@ucsd.edu>
2022-02-03 22:39:04 -0800

    added tests, lib.

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,2 +1,7 @@
 public class SkillDemo
-{}
+{
+  public static int subtract(int n1, int n2) // input n1 to be subtracted from n2
+  {
+    return n1-n2;
+  }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest 
+{
+  @Test
+  public void subtraction()
+  {
+    assertEquals(5,SkillDemo.subtract(3,1));
+  }    
+}
